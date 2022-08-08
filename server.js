bac8fde29be510a8aa355dd4bd59983c0f478e06
Maksim <maxkuku@gmail.com>
2022-08-08 13:23:41 +0300

    ДЗ 2 урока по HTTP
    Обработка POST создание файла, обработка DELETE удаления файла, Хранение куки авторизации

--- server.js
+++ server.js
@@ -6,14 +6,96 @@ const port = 8000;
 
 let fileline = '';
 
+// from https://stackoverflow.com/questions/3393854/get-and-set-a-single-cookie-with-node-js-http-server#answer-3409200
+function parseCookies (request) {
+    const list = {};
+    const cookieHeader = request.headers?.cookie;
+    if (!cookieHeader) return list;
+
+    cookieHeader.split(`;`).forEach(function(cookie) {
+        let [ name, ...rest] = cookie.split(`=`);
+        name = name?.trim();
+        if (!name) return;
+        const value = rest.join(`=`).trim();
+        if (!value) return;
+        list[name] = decodeURIComponent(value);
+    });
+
+    return list;
+}
+
+
+
+
+
 
 const requestListener = (req, res) => {
+
+const user = {
+    id: 123,
+    username: 'testuser',
+    password: 'qwerty'
+};
+
+
+
+const ifHasAuthCookies = (req, user) => {
+    let cookies = parseCookies(req)
+    if (cookies.authorized === 'true'
+            && cookies.userId === user.id.toString()){
+                return true;
+            }
+    return false;        
+}
+    
 
 
     if (req.url === '/delete') {
         if (req.method === 'DELETE') {
             res.writeHead(200);
-            res.end('success delete');
+
+
+            if (ifHasAuthCookies(req, user)) {
+
+                let deleteFile = '';
+                req.on('data', chunk => {
+                    string = chunk.toString();
+                    
+
+                    const searchParams = new URLSearchParams(string);
+                    for (const [key, value] of searchParams.entries()) {
+
+
+
+                        if(key === 'filename')
+                            deleteFile = `./files/${value}`;
+                        
+                    }
+
+
+                    if (fs.existsSync(deleteFile)) {
+                        fs.unlink(deleteFile, (err) => {
+                            if (err) {
+                                res.writeHead(400);
+                                res.end('Did not unlink file' + err);
+                                console.log(err);
+                            }
+                            
+                            console.log('deleted');
+                            res.end('success delete');
+                        })
+                    }
+                    else {
+                        res.writeHead(400);
+                        res.end('No such file' + deleteFile);
+                        console.log('No such file' + deleteFile);
+                    }
+                    
+                });
+
+            }
+
+            
         } else {
             res.writeHead(405);
             res.end('HTTP method not allowed for /delete');
@@ -25,6 +107,54 @@ const requestListener = (req, res) => {
     else if (req.url === '/post') {
         if (req.method === 'POST') {
             res.writeHead(200);
+
+
+            
+            
+            let string = '';
+            let filename = '';
+            let content = '';
+
+            if (ifHasAuthCookies(req, user)) {
+                
+
+                req.on('data', chunk => {
+                    string = chunk.toString();
+                
+
+                    const searchParams = new URLSearchParams(string);
+                    for (const [key, value] of searchParams.entries()) {
+
+                        // console.log(`${key} ${value}`)
+
+                        if(key === 'filename')
+                            filename = value;
+                        
+                        if(key === 'content')
+                            content = value;
+                    }
+
+                    // console.log(`filename: ${filename}, content: ${content}`)
+
+                    fs.writeFile( `./files/${filename}`, content, (err) => {
+                        if (err) {
+                            res.writeHead(404);
+                            console.log(err);
+                            res.end('File write error' + err);  
+                            
+                        }
+                        else {
+                            console.log("File written successfully\n");
+                            console.log("The written has the following contents:");
+                            console.log(fs.readFileSync(`./files/${filename}`, "utf8"));
+                        } 
+                    });
+
+                });
+
+            }
+
+
             res.end('success post');
         } else {
             res.writeHead(405);
@@ -45,7 +175,7 @@ const requestListener = (req, res) => {
     
             fileline = filenames.join(',');
     
-            console.log(fileline);
+       
             res.end(fileline);
         }
     }
@@ -59,6 +189,64 @@ const requestListener = (req, res) => {
         } else {
             res.writeHead(405);
             res.end('HTTP method not allowed for /redirect');
+        }
+    }
+
+
+
+    else if (req.url === '/auth') {
+        if (req.method === 'POST') {
+
+            
+
+            
+            let string = '';
+            let userTrue = false;
+            let userPassTrue = false;
+            req.on('data', chunk => {
+
+                
+                string = chunk.toString();
+
+                const searchParams = new URLSearchParams(string);
+                for (const [key, value] of searchParams.entries()) {
+
+                    
+                    
+                    if(key === 'username' && value === user.username){
+                        userTrue = true;
+                    }
+                    if(key === 'password' && value === user.password){
+                        userPassTrue = true;
+                    }
+                }
+
+                if(userTrue && userPassTrue){
+                    console.log('user true');
+                }
+
+                
+                if(userTrue && userPassTrue){
+                    res.writeHead(200, {
+                        'Content-Type': 'text/plain',
+                        'Set-Cookie': `userId=${user.id};MAX_AGE=172800;path=/;authorized=true;MAX_AGE=172800;path=/;`
+                    })
+                } 
+                
+
+
+            });
+
+            req.on('end', () => {
+                
+                res.end('success auth request');
+            });
+            
+
+            
+        } else {
+            res.writeHead(405);
+            res.end('HTTP method not allowed');
         }
     }
 
